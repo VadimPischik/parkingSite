9ba9b9c7cb281529543b3adf790365c69fa925ef
Georgy <georgy.tertychniy@yandex.ru>
2023-12-13 00:20:49 +0300

    refactor

--- server/parking/serializers.py
+++ server/parking/serializers.py
@@ -17,17 +17,21 @@ class ParkingSerializer(serializers.ModelSerializer):
     free_places =  serializers.SerializerMethodField("free")
     total_places =  serializers.SerializerMethodField("total")
 
+    def get_all(self, obj):
+        return mod.ParkingPlace.objects.filter(parking=obj)
+    
     def free(self, obj):
         handled = {i.place for i in 
-                   mod.Auto.objects.select_related("place").filter(place__parking = obj)}
-        all = {i for i in  mod.ParkingPlace.objects.exclude(parking=obj)}
+                   mod.Auto.objects.select_related("place").filter(place__parking = obj)
+                   }
+        all = {i for i in  self.get_all(obj)}
         free = all - handled
 
         return  [ParkingPlaceSerializer(i).data for i in free]
 
     def total(self, obj):
         return [ParkingPlaceSerializer(i).data for i in 
-                mod.ParkingPlace.objects.filter(parking=obj)]
+                self.get_all(obj)]
     
 
 class ClientSerializer(serializers.ModelSerializer):
